e0ed8596050f282504b059a0848f0cd8676f0279
Alex <rabbitix98@gmail.com>
2021-04-29 22:27:26 +0430

    add action

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    return {'hello': 'World'}
+
+
+@app.get('/fake_txt_process')
+def read_fake_text_process(txt: str):
+    data = {}
+    data['title'] = txt.title()
+    words = txt.split() # split with  whitespace
+    data['word_count'] = len(words)
+    data['sentence_count'] = txt.count('.') 
+    return data
+
+
+    
+
+  'name': 'virgool',
+        'runtime': 'python3.7',
+        'endpoint': 'https://erxzqx.deta.dev',
+        'visor': 'enabled',
+        'http_auth': 'disabled'
